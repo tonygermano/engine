852af8a5e7dc953d9b1a7f5fa0726ce8785ed51a
jdo <jdo@nextgen.com>
2024-04-29 12:17:35 -0700

    [ROCKSOLID-12661] Remove ISourceConnector reference in AbstractEmailReceiver

--- core-server-plugins/src/com/mirth/connect/donkey/server/channel/AbstractEmailReceiver.java
+++ core-server-plugins/src/com/mirth/connect/donkey/server/channel/AbstractEmailReceiver.java
@@ -1,5 +1,5 @@
 package com.mirth.connect.donkey.server.channel;
 
-public abstract class AbstractEmailReceiver implements ISourceConnector, PollSourceConnectorPlugin {
+public abstract class AbstractEmailReceiver implements PollSourceConnectorPlugin {
     public static Class<?> POLL_CONNECTOR;
 }

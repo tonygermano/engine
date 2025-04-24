a1b0293778b629f966e8e97fd521daf0abfc4db8
Tony Germano <tony@germano.name>
2025-04-24 09:33:16 -0400

    update project.clj to add license and remove native-image

--- launcher-server/engine/project.clj
+++ launcher-server/engine/project.clj
@@ -1,25 +1,12 @@
 (defproject org.openintegrationengine/engine "0.1.0-SNAPSHOT"
   :description "A native engine for launching Java applications with advanced .vmoptions support"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-v20.html"}
+  :url "https://github.com/OpenIntegrationEngine/engine"
+  :license {:name "Mozilla Public License Version 2.0"
+            :url "https://mozilla.org/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.12.0"]]
   :main org.openintegrationengine.engine.server.launcher
   :aot :all
 
-  ;; Add the plugin dependency
-  :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
+  :uberjar-name "engine.jar"
 
-  ;; Configuration for the native-image plugin
-  ;; The :name key here matches the :native-image map you had,
-  ;; but it's interpreted by the plugin.
-  :native-image {:name "engine"
-                 :opts ["--no-fallback"
-                        "--verbose"
-                        "--report-unsupported-elements-at-runtime"
-                        "--initialize-at-build-time"
-                        "--strict-image-heap"
-                        "-H:ReflectionConfigurationFiles=reflect-config.json"]}
-
-  :profiles {:dev {:resource-paths ["resources"]}
-             :native-image {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :profiles {:dev {:resource-paths ["resources"]}})
